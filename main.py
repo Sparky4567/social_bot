ac15e7d16ce86f55828ec4fd7f7e8078932dc4ac
Andrius Prartusis <andrius.pratusis1993@gmail.com>
2025-09-27 17:24:11 +0300

    exit | quit | q to exit

--- main.py
+++ main.py
@@ -7,27 +7,34 @@ def main():
     speakback = Speak_Module()
     llm = LLM_module()
     memory = MemoryDB()
-    if SPEECH_TO_TEXT:
-        asr = VoskRecognizer()
     try:
-        if SPEECH_TO_TEXT and asr.is_ready:
-            print("\n\nListening for your input...\n\n")
-            user_input = asr.listen_until_stop().strip()
-            print("You said: {}".format(user_input))
+        if SPEECH_TO_TEXT:
+            asr = VoskRecognizer()
+            if asr.is_ready:
+                print("\n\nListening for your input...\n\n")
+                user_input = asr.listen_until_stop().strip()
+                print("You said: {}".format(user_input))
         else:
             user_input = str(input("\n\nYour input:\n\n")).strip()
-        response = llm.get_response_from_llm(user_input)
-        memory._create_table()
-        if "<think>" in response:
-            bot_response = str(response).split("</think>")[1].strip()
-            print("===\n\n",bot_response,"\n\n===\n\n")
-        else:
-            bot_response = response
-            print("===\n\n",bot_response,"\n\n===\n\n")
-        print("\n\nStoring in memory... User-input: {} Bot-response: {}\n\n".format(user_input, bot_response))
-        memory.add_memory(user_input, bot_response)
-        speakback.speak(bot_response)
-        main()
+        match user_input.lower():
+            case "exit" | "quit" | "q":
+                print("Quiting...")
+                quit()
+            case _:
+                print("Processing your input...")
+                response = llm.get_response_from_llm(user_input)
+                memory._create_table()
+                bot_response = "No response"
+                if "<think>" in response:
+                    bot_response = str(response).split("</think>")[1].strip()
+                    print("===\n\n",bot_response,"\n\n===\n\n")
+                else:
+                    bot_response = str(response).strip()
+                    print("===\n\n",bot_response,"\n\n===\n\n")
+                print("\n\nStoring in memory... User-input: {} Bot-response: {}\n\n".format(user_input, bot_response))
+                memory.add_memory(user_input, bot_response)
+                speakback.speak(bot_response)
+                main()
     except Exception as e:
         print(str("Exception: {}".format(e)))
     except KeyboardInterrupt:
